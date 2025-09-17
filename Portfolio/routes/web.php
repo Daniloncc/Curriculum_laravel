cc003fe99cc9ef2839e52ae285c0b7f8792384e0
Danilo Costa <dancc86@gmail.com>
2025-09-17 19:25:45 -0400

    Creation des routes dans le fichier routes/web.php

--- Portfolio/routes/web.php
+++ Portfolio/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PortfolioController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PortfolioController::class, 'index']);
+Route::get('/home', [PortfolioController::class, 'index']);
+Route::get('/about', [PortfolioController::class, 'about']);
+Route::get('/projects', [PortfolioController::class, 'projects']);
+Route::get('/contact', [PortfolioController::class, 'contact']);
+Route::post('/contact', [PortfolioController::class, 'contactForm']);
